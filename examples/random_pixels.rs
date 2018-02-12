8f690daa1fb83bdf6df490575347c1b3277a41c0
Nicodemus E Paradiso <nicodemus26@gmail.com>
2018-02-12 14:16:40 -0800

    Extract tcp connection and rng

--- examples/random_pixels.rs
+++ examples/random_pixels.rs
@@ -1,31 +1,24 @@
+extern crate easyopc;
 extern crate rand;
 
 use rand::Rng;
-use std::env;
-use std::io::prelude::*;
-use std::net::{TcpStream, Shutdown};
+use std::io::Write;
 use std::time::Duration;
 
 fn main() {
-    let endpoint = env::var("OPC_ENDPOINT")
-        .unwrap_or(String::from("127.0.0.1:7890"));
-    let mut stream = TcpStream::connect(endpoint).unwrap();
-    stream.shutdown(Shutdown::Read).unwrap(); // Not a great listener...
-    stream.set_nodelay(true).unwrap();
-    let mut rng = rand::thread_rng();
-
+    let mut ctx = easyopc::Connection::default();
     loop {
-        let mut message = vec![0u8; 4+(512*3)];
+        let mut message = vec![0u8; 4 + (512 * 3)];
         // Command and channel both 0.
-        message[2] = ((512u16*3) >> 8)  as u8; // Length high byte
-        message[3] = ((512u16*3) & 255) as u8; // Length low byte
-        
-        for i in 0..(512*3) {
-            message[i+4] = rng.gen();
+        message[2] = ((512u16 * 3) >> 8) as u8; // Length high byte
+        message[3] = ((512u16 * 3) & 255) as u8; // Length low byte
+
+        for i in 0..(512 * 3) {
+            message[i + 4] = ctx.rng.gen();
         }
-        
-        stream.write_all(&message).unwrap();
-        
+
+        ctx.stream.write_all(&message).unwrap();
+
         std::thread::sleep(Duration::from_millis(1000));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+extern crate rand;
+
+use std::net::{Shutdown, TcpStream};
+use std::env;
+use std::default::Default;
+
+pub struct Connection {
+    pub rng: rand::ThreadRng,
+    pub stream: TcpStream,
+}
+
+impl Default for Connection {
+    fn default() -> Connection {
+        let endpoint = env::var("OPC_ENDPOINT").unwrap_or(String::from("127.0.0.1:7890"));
+        let stream = TcpStream::connect(endpoint).unwrap();
+        stream.shutdown(Shutdown::Read).unwrap(); // Not a great listener...
+        stream.set_nodelay(true).unwrap();
+        let rng = rand::thread_rng();
+        Connection {
+            rng: rng,
+            stream: stream,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
